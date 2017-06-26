eadb10597b97a8732bec2c08597699f2ebb1ecb0
David Polania <davidpolaniaac@users.noreply.github.com>
2017-06-26 22:55:42 +0000

    script del scanner

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /var/www/html
 sudo git pull origin master
-./home/root/sonar-scanner-3.0.3.778-linux/bin/sonar-scanner
+pwd
+echo 'qeu paso'
+sudo ./home/root/sonar-scanner-3.0.3.778-linux/bin/sonar-scanner
 sudo service apache2 restart
